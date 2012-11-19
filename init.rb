9d80bb61e60c4171340267bce9bdd0090259244f
Maximilian Schoening <mschoening@me.com>
2012-11-19 02:21:26 -0800

    Use a private method to define values

--- init.rb
+++ init.rb
@@ -2,19 +2,6 @@ require "heroku/command/base"
 
 class Heroku::Command::Values < Heroku::Command::Base
 
-  # values
-  #
-  # our company values
-  #
-  def values
-    [
-      "Simple beats easy.",
-      "Do less, but better.",
-      "Delight the user.",
-      "Good enough isn't."
-    ]
-  end
-
   def index
     puts values[rand(values.length)]
   end
@@ -24,4 +11,19 @@ class Heroku::Command::Values < Heroku::Command::Base
       puts value
     end
   end
+  
+  private
+    # values
+    #
+    # our company values
+    #
+    def values
+      [
+        "Simple beats easy.",
+        "Do less, but better.",
+        "Delight the user.",
+        "Good enough isn't."
+      ]
+    end
+        
 end
